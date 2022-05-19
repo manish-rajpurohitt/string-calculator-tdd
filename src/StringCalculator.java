8a18d43f380c9f8268edbb233d09377f90fcb585
Manish Kumar <manishkumar9866@gmail.com>
2022-05-19 19:54:04 +0530

    messy code need refactoring

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,8 +4,14 @@ import java.util.List;
 public class StringCalculator {
     public int Add(String numbers){
         if(!numbers.isEmpty()){
+            String delimiter = "";
+            if(numbers.startsWith("//")){
+                delimiter = String.valueOf(numbers.split("\n")[0].charAt(2));
+                numbers = numbers.split("\n")[1];
+            }
             List<Integer> integersList = new ArrayList<>();
-            String[] stringsArrayOfNumbers = numbers.split(",|\n");
+
+            String[] stringsArrayOfNumbers = delimiter.equals("") ? numbers.split("[,\n]"): numbers.split(delimiter);
             int sum = 0;
             for (String stringsArrayOfNumber : stringsArrayOfNumbers) {
                 sum += Integer.parseInt(stringsArrayOfNumber);
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -12,6 +12,10 @@ public class StringCalculatorTest {
         assertEquals(calculator.Add("1,2,3,4,5,6,7,8,9,10,100,200"),355);
         assertEquals(calculator.Add("1\n2,3"),6);
         assertEquals(calculator.Add("1\n2,3\n4\n5\n6\n1,4,5,6,7,8\n8"),60);
+        assertEquals(calculator.Add("//;\n1;2;3;1;4;5;6;7;8;8"),45);
+        assertEquals(calculator.Add("//;\n1;2;3;14;4;8"),32);
+        assertEquals(calculator.Add("//'\n4'5'6'7'8'8"),38);
+        assertEquals(calculator.Add("//@\n4@5@6@7@8@8"),38);
 
     }
 }
