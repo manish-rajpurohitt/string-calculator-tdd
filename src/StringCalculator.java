fe36e8e9242d659a426c3b05a435767e5b8a81c4
Manish Kumar <manishkumar9866@gmail.com>
2022-05-19 19:08:17 +0530

    passed some test cases

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
     public int Add(String numbers){
-        return 0;
+        if(!numbers.isEmpty()){
+            List<Integer> integersList = new ArrayList<>();
+            String[] stringsArrayOfNumbers = numbers.split(",");
+            int sum = 0;
+            for (String stringsArrayOfNumber : stringsArrayOfNumbers) {
+                sum += Integer.parseInt(stringsArrayOfNumber);
+            }
+            return sum;
+        }else return 0;
     }
 }
 
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -7,5 +7,8 @@ public class StringCalculatorTest {
     @Test
     public void TestCalculator(){
         assertEquals(calculator.Add(""),0);
+        assertEquals(calculator.Add("1"),1);
+        assertEquals(calculator.Add("1,2"),3);
+        assertEquals(calculator.Add("1,2,3,4,5,6,7,8,9,10,100,200"),355);
     }
 }
